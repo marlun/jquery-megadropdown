0320e2cf446687a82f6646d28f0b8f977d8ebca4
Martin Lundberg <martin.lundberg@gmail.com>
2010-10-28 14:35:55 +0200

    Changed into using the module pattern.
    I'm not sure if this is what we want but lets try it and see if it feels
    good.

--- jquery.megadropdown.js
+++ jquery.megadropdown.js
@@ -1,54 +1,99 @@
+var MegaDropDown = (function ($) {
+	var pub = {};
+	var selectElement;
+	var megaddSelectBox;
+	var items = [];
+
+	//
+	// SelectBox
+	//
+
+	function buildSelectBox() {
+		var selected = getOriginalSelected();
+		var control = $('<div>');
+		control.html(selected.text);
+		control.addClass('megadd-select');
+		control.bind('click.megaDropDown', selectBoxClickHandler);
+
+		return control;
+	}
+
+	function selectBoxClickHandler(e) {
+		var listview = buildListView();
+		megaddSelectBox.after(listview);
+	}
+
+	function listViewItemClickHandler(e) {
+		console.log("Clicked element value: " + $(this).data('value'));
+		// 6.1 Set the correct values on the original select
+		//var selectElement = $(this).closest('select');
+		//selectElement.val($(this).data('value'));
+		// 6.2 Update MegaDropDown control to show the selected value
+		//var megaDropDownSelect = $(this).closest('.megaadd-select');
+		//megaDropDownSelect.html($(this).html());
+	}
+
+	//
+	// Listview
+	//
+
+	function buildListView() {
+		var control = $('<div>');
+		control.addClass('megadd-list');
+		
+
+		for (var i = 0; i < items.length; i++) {
+			var item = $('<p>');
+			item.html(items[i].text);
+			item.data('value', items[i].key);
+			item.bind('click.megaDropDown', listViewItemClickHandler);
+			control.append(item);
+		}
+
+		return control;
+	}
+
+	//
+	// General functions
+	//
+
+	function getOriginalItems() {
+		var elements = selectElement.find('option');
+
+		elements.each(function (idx, element) {
+			items.push({'text' : $(element).html(), 'key' : $(element).val()});
+		});
+	}
+
+	function getOriginalSelected() {
+		var selected = selectElement.find("option[value='" + selectElement.val() + "']:first");
+		return {'text' : selected.html(), 'key' : selected.val()};
+	}
+
+	//
+	// Public methods
+	//
+
+	pub.show = function () {
+		megaddSelectBox = buildSelectBox();
+		selectElement.after(megaddSelectBox);
+	};
+
+	pub.init = function (element) {
+		// Save the original select element for later use
+		selectElement = element;
+		selectElement.hide();
+		getOriginalItems();
+	};
+
+	return pub;
+}(jQuery));
+
 (function($) {
 	$.fn.megaDropDown = function() {
-
 		return this.each(function() {
-
-			var $this = $(this);
-			var selectElement = $this;
-			var value = $this.val();
-			var selected = $this.find("option[value='" + value + "']:first");
-			var optionElements = $this.find("option");
-
-			// 1. Hide the old HTML control
-			$this.hide();
-
-			// 2. Show our control's HTML
-			var control = $('<div>' + selected.html() + '</div>');
-			control.addClass('megadd-select');
-			$this.after(control);
-
-			// 3. Connect namespaced click event on our control
-			// 4. On click: Show the MegaDropDown list
-			control.bind("click.megaDropDown", function () {
-				console.log("marlun clicked the control");
-
-				var view = $('<div>');
-				view.addClass('megadd-list');
-
-				// Add all of the select options as items in the list
-				optionElements.each(function (idx, optionElement) {
-					var item = $('<p>');
-					item.html($(optionElement).html());
-					item.data('value', $(optionElement).val());
-
-					// 5. Connect events to the items in the list
-					item.bind('click.megaDropDown', function () {
-						console.log("Clicked element value: " + $(this).data('value'));
-						// 6. On click event
-						// 6.1 Set the correct values on the original select
-						var selectElement = $(this).closest('select');
-						selectElement.val($(this).data('value'));
-						// 6.2 Update MegaDropDown control to show the selected value
-						var megaDropDownSelect = $(this).closest('.megaadd-select');
-						megaDropDownSelect.html($(this).html());
-					});
-
-					view.append(item);
-				});
-
-				// Show the list after the dropdown control
-				$(this).after(view);
-			});
+			MegaDropDown.init($(this));
+			MegaDropDown.show();
 		});
 	};
-})( jQuery );
+})(jQuery);
