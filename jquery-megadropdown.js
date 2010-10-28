7ee758f9313e9b9d33d0e1e4db4be48b1e4d2e93
Martin Lundberg <martin.lundberg@gmail.com>
2010-10-28 10:46:00 +0200

    Changed the comments to english.

--- jquery-megadropdown.js
+++ jquery-megadropdown.js
@@ -1,35 +1,37 @@
 (function($) {
 	$.fn.megaDropDown = function() {
 
-    return this.each(function() {
+		return this.each(function() {
 
-      var $this = $(this);
-      var value = $this.val();
-      var selected = $this.find("option[value='" + value + "']:first");
+			var $this = $(this);
+			var value = $this.val();
+			var selected = $this.find("option[value='" + value + "']:first");
 			var options = $this.find("option");
-						
-			// 1. Dölja $this
+
+			// 1. Hide the old HTML control
 			$this.hide();
-			
-			// 2. Lägga in ny html på selectens plats
+
+			// 2. Show our control's HTML
 			var control = $('<div>' + selected.html() + '</div>');
 			$this.after(control);
-			
-			// 3. Koppla namespacat event till nya html koden
-			// 4. Vid klick/event vissa själva listan
+
+			// 3. Connect namespaced click event on our control
+			// 4. On click: Show the MegaDropDown list
 			control.bind("click.megaDropDown", function (){
 				console.log("marlun clicked the control");
-				
-				var view = $('<div></div>');
-				
-			});
-						
-			
-			// 5. Koppla event till alternativ i listan
-			
-			// 6. Fånga event och sätta värdet som klickades i den dolda selecten, och uppdatera vår control
-      
-    });
 
-  };
+				var view = $('<div></div>');
+
+			});
+
+
+			// 5. Connect events to the items in the list
+
+			// 6. On click event
+			// 6.1 Set the correct values on the original select
+			// 6.2 Update MegaDropDown control to show the selected value
+
+		});
+
+	};
 })( jQuery );
